047c2cff66a8dd51fe3ce6c15a7dffd8a801303f
egorn <egor.nazarenko31@mail.ru>
2022-12-03 12:02:13 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace labe5._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
